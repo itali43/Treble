c8505b26b01e718d1692feb7ce60b2cb516f384c
Elliott <itali43@gmail.com>
2018-08-16 00:14:35 -0700

    playlist

--- GrandComposer/AppDelegate.swift
+++ GrandComposer/AppDelegate.swift
@@ -22,40 +22,25 @@ import AVFoundation
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
+    let menu = NSMenu()
     let statusItem = NSStatusBar.system.statusItem(withLength: -2)
-    var player = AVAudioPlayer()
+    var player = AVQueuePlayer()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        /*TESTING AREA*/
+        /*testing area*/
+
         if let button = statusItem.button {
-            button.image = #imageLiteral(resourceName: "trebSmall")
+            button.image = #imageLiteral(resourceName: "trebSmall") // treble white image
             button.target = self
             button.action = #selector(play(sender:))
             button.sendAction(on: [.leftMouseUp, .rightMouseUp, .rightMouseDragged])
-
-
-            
-            
         }
         print("finish launching")
 
-        
-        
-        
+//            menu.addItem(NSMenuItem(title: "Quit Treble", action: #selector(self.applicationWillTerminate(_:)), keyEquivalent: "q"))
+//            statusItem.menu = menu
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -66,44 +51,70 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let event = NSApp.currentEvent!
         
         if event.type == NSEvent.EventType.rightMouseUp {
+            // right clicks!
+            
             print("Right click")
             stopSound(file: "NewWorldOne", ext: "mp3")
-        } else if event.type == NSEvent.EventType.rightMouseDragged {
-            print("right mouse drag click")
-            stopSound(file: "NewWorldOne", ext: "mp3")
-            print("exiting")
-//            NSApplication.terminate()
-            NSApp.terminate(self)
+            if event.clickCount == 2 {
+                print("quit? \(event.clickCount)")
+            } else {
+                print("click not a 2! -  \(event.clickCount)")
+            }
+//        } else if event.type == NSEvent.EventType.rightMouseDragged {
+//            print("right mouse drag click")
+//            stopSound(file: "NewWorldOne", ext: "mp3")
+//            print("exiting")
+            
         }  else {
-    print("Left click")
-    playSound(file: "NewWorldOne", ext: "mp3")
-    
+            // prob left clicks
+            print("Left click: \(event.clickCount)")
+//    playSound(file: "NewWorldOne", ext: "mp3")
+            playSoundFrom(directory: ["NewWorldOne"], ext: "mp3")
     }
-
-        
         print("hi")
     }
 
-    
-    func playSound(file:String, ext:String) -> Void {
-        let url = Bundle.main.url(forResource: file, withExtension: ext)!
+    func playSoundFrom(directory: Array<String>, ext:String) -> Void {
+//        let item0 = AVPlayerItem.init(url: Bundle.main.url(forResource: "AirHorn", withExtension: "wav")!)
+        let item1 = AVPlayerItem.init(url: Bundle.main.url(forResource: "NewWorldOne", withExtension: "mp3")!)
+        let item2 = AVPlayerItem.init(url: Bundle.main.url(forResource: "NewWorldTwo", withExtension: "mp3")!)
+        let item3 = AVPlayerItem.init(url: Bundle.main.url(forResource: "NewWorldThree", withExtension: "mp3")!)
+        let item4 = AVPlayerItem.init(url: Bundle.main.url(forResource: "NewWorldFour", withExtension: "mp3")!)
+
+        let itemsToPlay:[AVPlayerItem] = [item1, item2, item3, item4]
         do {
-            player = try AVAudioPlayer(contentsOf: url) // unsafe
-//            guard let player = newplayer else { return }
-            
-            player.prepareToPlay()
+            player = try AVQueuePlayer.init(items: itemsToPlay) //AVAudioPlayer(contentsOf: url) // unsafe
+            //            guard let player = newplayer else { return }
             player.play()
         } catch let error {
             print(error.localizedDescription)
         }
     }
+
+    
+//    func playSound(file:String, ext:String) -> Void {
+//        let url = Bundle.main.url(forResource: file, withExtension: ext)!
+//        do {
+//            player = try AVAudioPlayer(contentsOf: url) // unsafe
+////            guard let player = newplayer else { return }
+//
+//            player.prepareToPlay()
+//            player.play()
+//        } catch let error {
+//            print(error.localizedDescription)
+//        }
+//    }
+//
+    
     
     func stopSound(file:String, ext:String) -> Void {
-        let url = Bundle.main.url(forResource: file, withExtension: ext)!
+//        let url = Bundle.main.url(forResource: file, withExtension: ext)!
         do {
-            player = try AVAudioPlayer(contentsOf: url) // unsafe
+//            player = try AVAudioPlayer(contentsOf: url) // unsafe
             //            guard let player = newplayer else { return }
-            player.stop()
+            player.pause()
+            player = AVQueuePlayer()
+
 //            player.prepareToPlay()
 //            player.play()
         } catch let error {
@@ -146,6 +157,23 @@ class AppDelegate: NSObject, NSApplicationDelegate {
  }
  
  
+ 
+ FILEMANAGER FOOLISHNESS:
+ 
+ let home = FileManager.default.homeDirectoryForCurrentUser
+ let playgroundPath = "Documents/treblePlays"
+ let playgroundUrl = home.appendingPathComponent(playgroundPath)  //        let completeUrl = URL(fileURLWithPath: completePath)
+ print(playgroundUrl)
+ let fileManager = FileManager.default
+ let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+ do {
+ let fileURLs = try fileManager.contentsOfDirectory(at: playgroundUrl, includingPropertiesForKeys: nil)
+ // process files
+ print("files")
+ print(fileURLs)
+ } catch {
+ print("Error while enumerating files \(documentsURL.path): \(error.localizedDescription)")
+ }
  
  
  */
